1545e47c3551c33e51a91c944ade8d2ae53eb4df
Joseph Hill <joseph.hill@uconn.edu>
2017-01-02 12:43:57 -0500

    Create Parser.js

---
+++ Parser.js
@@ -0,0 +1,18 @@
+var data;
+
+function handleFileSelect(evt) {
+  var file = evt.target.files[0];
+
+  Papa.parse(file, {
+    header: true,
+    dynamicTyping: true,
+    complete: function(results) {
+      data = results;
+      console.log(results);
+    }
+  });
+}
+
+$(document).ready(function(){
+  $("#csv-file").change(handleFileSelect);
+});
