7c9ebbdc7426ee8829ce0acbe5c657975d93a90b
Joseph Hill <joseph.hill@uconn.edu>
2017-01-04 15:03:10 -0500

    Update MapScript.js

--- MapScript.js
+++ MapScript.js
@@ -1,23 +1,12 @@
 var map;
 var myLocation = {lat: 43.3095, lng: -73.644};
 
-function addMarker(lt, ln, prov)
-{
-  console.log("Latitude: " + lt + ". Longitude: " + ln);
-  var location = {lat: lt, lng: ln};
-  var marker = new google.maps.Marker({
-    position: location,
-    map: map
-  });
-  marker.setMap(map);
-}
 
 function initMap() {
-  var myLatLng = myLocation;
 
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
-    center: myLatLng,
+    center: myLocation,
     rotateControl: true
   });
 
@@ -28,9 +17,16 @@ function initMap() {
     title: 'Hello World!'
   }); */
 
-  /*var infoWindow = new google.maps.InfoWindow({content:"Hello"});
-  google.maps.event.addListener(marker,'click',function(e){
+}
 
-    infoWindow.open(map, marker);
-    });*/
+function addMarker(lt, ln, prov)
+{
+  // Troubleshooting call:
+  console.log("Latitude: " + lt + ". Longitude: " + ln);
+
+  var location = {lat: lt, lng: ln};
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(lt, ln),
+    map: map
+  });
 }
