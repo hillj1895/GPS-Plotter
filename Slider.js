c1f1b8db6a74b6b2d019f302d0ef5f588c1d04da
Joseph Hill <joseph.hill@uconn.edu>
2017-01-10 14:30:11 -0500

    Update Slider.js

--- Slider.js
+++ Slider.js
@@ -1,8 +1,7 @@
 function addSlider(fileData) {
 	var minDate = new Date(parseInt(fileData.data[0].senseStartTimeMillis));
-	var maxDate = new Date(parseInt(fileData.data[data.data.length - 2].senseStartTimeMillis));
-	console.log(minDate);
-	console.log(maxDate);
+	var maxDate = new Date(parseInt(fileData.data[data.data.length - 1].senseStartTimeMillis));
+
 	$("#slider").dateRangeSlider({
 
 	  arrows: false,
@@ -21,7 +20,8 @@ function addSlider(fileData) {
 	document.getElementById("sliderHeading").innerHTML = "Specify a Date Range:";
 
 	// Will call an updateMarkers() function that adds/removes points depending on their date
-	$("#slider").bind("valuesChanged", function(e, data){
-	  console.log("Values just changed. min: " + data.values.min + " max: " + data.values.max);
+	$("#slider").bind("valuesChanged", function(e, data) {
+		updateMarkers(data.values.min, data.values.max);
+	  //console.log("Values just changed. min: " + data.values.min + " max: " + data.values.max);
 	});
 }
