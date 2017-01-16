de7f1fd5e22bdcbb3a70da425fa63f734be0caf0
Joseph Hill <joseph.hill@uconn.edu>
2017-01-16 14:18:13 -0500

    Update Parser.js

--- Parser.js
+++ Parser.js
@@ -7,28 +7,14 @@ function handleFile(evt) {
     dynamicTyping: true,
     complete: function(results) {
       data = results;
-      console.log(results.data);
-      addSlider(data);
       updateMapCenter(data);
       addMarkers(data);
+      classify(data);
+      addSlider(data);
     }
   });
 }
 
-// This method is only called when the initial CSV file is uploaded
-function addMarkers(data) {
-  for (var i = 0; i<data.data.length ; i++) {
-    var lat = data.data[i].latitude;
-    var lon = data.data[i].longitude;
-    var prov = data.data[i].Provider;
-    var timeMillis = data.data[i].senseStartTimeMillis;
-    if (lat !== undefined & lon !== undefined) {
-        addMarker(lat, lon, prov, timeMillis);
-    }
-  }
-}
-
-$(document).ready(function()
-{
+$(document).ready(function() {
   $("#csv-file").change(handleFile);
 });
